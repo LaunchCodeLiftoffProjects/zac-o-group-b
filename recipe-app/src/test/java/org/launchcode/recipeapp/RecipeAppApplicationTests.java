f4c53bce5812d0ce204a5f1ad0fd5c5919bd02ef
Salini Arjunan <salini_arjunan@gmail.com>
2021-08-15 21:54:25 -0500

    Hello World Commit

---
+++ recipe-app/src/test/java/org/launchcode/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
