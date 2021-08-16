3a22dd33503ca1dca8b447dc4548207253ea2acc
PCN8 <prismcortex@outlook.com>
2021-08-16 14:56:44 -0500

    First commit

---
+++ src/main/java/com/prismcortex/stayfreshfridge/StayfreshfridgeApplication.java
@@ -0,0 +1,13 @@
+package com.prismcortex.stayfreshfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayfreshfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayfreshfridgeApplication.class, args);
+	}
+
+}
