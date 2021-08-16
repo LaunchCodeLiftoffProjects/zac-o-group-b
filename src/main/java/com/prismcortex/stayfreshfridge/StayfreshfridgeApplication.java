d1ff1ba0f222731a9776b4b77e48fa4d625a8407
PCN8 <prismcortex@outlook.com>
2021-08-16 15:51:06 -0500

    created landing page

--- src/main/java/com/prismcortex/stayfreshfridge/StayfreshfridgeApplication.java
+++ src/main/java/com/prismcortex/stayfreshfridge/StayfreshfridgeApplication.java
@@ -2,12 +2,25 @@ package com.prismcortex.stayfreshfridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class StayfreshfridgeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StayfreshfridgeApplication.class, args);
+	}
+
+	@Controller
+	public class HomePage {
+
+		@RequestMapping("")
+		public String underConstuction (Model model) {
+			model.addAttribute("title", "Stay Fresh Fridge Coming Soon...");
+			return "comingsoon";
+		}
 	}
 
 }
