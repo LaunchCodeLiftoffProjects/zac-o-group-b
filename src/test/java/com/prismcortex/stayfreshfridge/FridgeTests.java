32fb7857cb641830910391d7a915e6def42f2076
PCN8 <prismcortex@outlook.com>
2021-08-25 11:57:09 -0500

    Changed FoodItem to GroceryItem and tested and created addItem and delete item method in ShoppingList.

--- src/test/java/com/prismcortex/stayfreshfridge/FridgeTests.java
+++ src/test/java/com/prismcortex/stayfreshfridge/FridgeTests.java
@@ -1,29 +1,28 @@
 package com.prismcortex.stayfreshfridge;
 
-import com.prismcortex.stayfreshfridge.models.FoodItem;
+import com.prismcortex.stayfreshfridge.models.GroceryItem;
 import com.prismcortex.stayfreshfridge.models.Fridge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.ArrayList;
 
 @SpringBootTest
 public class FridgeTests {
 
-    FoodItem testFoodItem = new FoodItem();
-    FoodItem testFoodItem1 = new FoodItem();
+    GroceryItem testGroceryItem = new GroceryItem();
+    GroceryItem testGroceryItem1 = new GroceryItem();
 
     Fridge testFridge = new Fridge();
     // Write before each that creates two food items and puts them in an ArrayList
     @BeforeEach
     public void createFoodAndList() {
-        testFoodItem.setName("pork");
-        testFoodItem.setExpires("8/22/21");
-        testFoodItem1.setName("beans");
-        testFoodItem1.setExpires("8/22/21");
-        testFridge.addFoodItem(testFoodItem);
-        testFridge.addFoodItem(testFoodItem1);
+        testGroceryItem.setName("pork");
+        testGroceryItem.setExpires("8/22/21");
+        testGroceryItem1.setName("beans");
+        testGroceryItem1.setExpires("8/22/21");
+        testFridge.addFoodItem(testGroceryItem);
+        testFridge.addFoodItem(testGroceryItem1);
 
     }
 
